6d4ecebf31d3e6220e68ee208e5aa27a5fef9278
MrDmishra <mr.dmishra0003@gmail.com>
2022-01-23 21:48:29 +0530

    Authentication & Authorization

---
+++ src/main/java/com/olo/ldap_access/LdapAccessApplication.java
@@ -0,0 +1,13 @@
+package com.olo.ldap_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olo/ldap_access/LdapAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olo.ldap_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
